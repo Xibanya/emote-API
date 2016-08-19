e65ece196cd9b830323cdbf14a35308181cc3f6c
Manuela Malasaña <Xibanya@users.noreply.github.com>
2016-08-19 18:49:47 -0500

    Update app.rb
    get '/emote' do
      status 200
      headers \
         'Content-Type' => 'application/json' 
      page = Nokogiri::HTML(open('http://forums.somethingawful.com/misc.php?action=showsmilies))
      search = ':' + params['text'] + ':'
      items = page.css('div.text')
      items.each do |item|
        if item.text =~ /#{search}/
          @emote_path = item.next_element['esrc'].to_s
        end
      end
      body '{"response_type": "in_channel","text": "' + @emote_path + '"}'
    end

--- app.rb
+++ app.rb
@@ -15,9 +15,11 @@ end
   
 get '/emote' do
    search = (':' + params['text'] + ':').to_s
+   page = Nokogiri::HTML(open('http://forums.somethingawful.com/misc.php?action=showsmilies))
    puts 'a GET for ' + search
+   items = page.css('div.text')
    status 200
    headers \
    'Content-Type' => 'application/json'
-   body '{ "test": "#{search}" }'
+   body '{ "response_type": "in_channel", "text": "insert search results here" }'
 end
