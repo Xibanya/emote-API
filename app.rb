203c4a5581eae30c87c5a3c39d09b45cd32e8468
Manuela Malasaña <Xibanya@users.noreply.github.com>
2016-08-19 17:10:42 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'nokogiri'
 #set :port, 3000
 
 get '/' do
+  puts 'Basic GET'
   status 200
   headers \
     'Content-Type' => 'application/json'
@@ -12,11 +13,13 @@ get '/' do
 end
 
 get '/emote' do
+  puts 'Looking for emote ' + params['text']
   page = Nokogiri::HTML(open('http://forums.somethingawful.com/misc.php?action=showsmilies'))
   search = ':' + params['text'] + ':'
   items = page.css('div.text')
   items.each do |item|
     if item.text =~ /#{search}/
+      status 200
       headers \
         'Content-Type' => 'application/json'
       body '{"response_type": "in_channel","text": "' + item.next_element['src'].to_s + '"}'
