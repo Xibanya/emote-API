ddd288681ab20c47cea2932f111f1a9bd024f70e
Manuela Malasaña <Xibanya@users.noreply.github.com>
2016-08-19 19:09:33 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -19,13 +19,13 @@ get '/emote' do
    puts 'a GET for ' + search
    items = page.css('div.text')
      items.each do |item|
-      if item.text =~ /search/
+      if item.text =~ /#{search}/
         puts 'found it'
-        
+        @img = item.next_element['src'].to_s
       end
      end
    status 200
    headers \
    'Content-Type' => 'application/json'
-   body '{ "response_type": "in_channel", "text": "insert search results here" }'
+   body '{ "response_type": "in_channel", "text": "#{@img}" }'
 end
